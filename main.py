b21b9a97dee34fcae5c18fe1a95ea3685f5c9258
Aratar <dead.baha.31@gmail.com>
2023-03-23 12:11:48 +0600

    image

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 import json
 import requests
 from bs4 import BeautifulSoup
+from io import BytesIO
 
 bot = telebot.TeleBot('6215252216:AAGgcdkwjwM0oWlJvzAYmoEKXt0N2oaOwSY')
 
@@ -14,7 +15,7 @@ def parse_website(message):
     url = 'http://35.234.109.231/api/post/'
     response = requests.get(url)
     data = json.loads(response.text)
-    owner, descr = '', ''
+    owner, descr, med = '', '', ''
     for i in data:
         for k, v in i.items():
             if k == 'owner':
@@ -22,9 +23,21 @@ def parse_website(message):
 
             elif k == 'descriptions':
                 descr = v
+            
+            elif k == 'media':
+                for j in v:
+                    for k2, v2 in j.items():
+                        if k2 == 'media':
+                            med = v2
 
-            if owner and descr:
+            if owner and descr and med:
+                response = requests.get(med)
+                photo = BytesIO(response.content)
+                bot.send_photo(message.chat.id, photo,caption=f'Пост пользователя: {owner}\n{descr}')
+                owner, descr, med = '', '', ''
+
+            elif owner and descr:
                 bot.send_message(message.chat.id, f'Пост пользователя: {owner}\n{descr}')
-                owner, descr = '', ''
+                owner, descr, med = '', '', ''
 
 bot.polling()
